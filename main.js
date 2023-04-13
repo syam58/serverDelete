296f1a0be284c12d95d06649fa330ccc36579559
syam v <64647318+syam58@users.noreply.github.com>
2023-04-13 09:31:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,46 +2,39 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
-let users=[]
-
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
 })
-app.get('/',(req,res)=>{
-  res.send("Server working...")
-})
 
 io.on("connection", (socket) => {
-  console.log("new client connected")
-  
-	socket.emit("me", socket.id)
-  users.push(socket.id)
-  console.log('socket entered ' + socket.id)
-  socket.on("joinRoom",(room)=>{
-    socket.join(room)
-    io.emit("allUsers",users)
-  })
-  
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
+	  console.log("connection established");
+	
+	
+	socket.on("join-room", (data) => {
+	  console.log(data);
+	  
+		socket.join(data.room);
 	})
 
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from })
+	socket.on("send-message", (data) => {
+		console.log(data);
+		
+		socket.to(data.room).emit("receive-message", {name:data.name,message:data.message})
 	})
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
+	socket.on("upload", (data) => {
+		socket.to(data.room).emit("receive-upload", {name:data.name,message:data.message,type:data.type})
 	})
 	
-	socket.on("send-message",(data)=>{
-	  console.log("server received");
-	  socket.broadcast.emit("receive-message",data)
+
+	socket.on("disconnect", () => {
+		//socket.broadcast.emit("callEnded")
 	})
 	
 })
 
 server.listen(5000, () => console.log("server is running on port 5000"))
+  
+	
